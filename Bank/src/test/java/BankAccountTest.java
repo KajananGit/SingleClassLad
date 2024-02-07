eb84d0c06cd716e91bdcf06df6282f059b13c727
Kajanan <Kajanan15i@gmail.com>
2024-02-07 15:12:24 +0000

    Created canGetFirstName() method to test the getFirstName() method

--- Bank/src/test/java/BankAccountTest.java
+++ Bank/src/test/java/BankAccountTest.java
@@ -1,4 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
 
+    private BankAccount bankAccount;
 
+    @BeforeEach
+    public void setUp(){
+        bankAccount = new BankAccount("Bruce", "Wayne", "23/07/1999", 3943);
+    }
+
+    @Test
+    public void canGetFirstName() {
+        String expected = "Bruce";
+        String actual = bankAccount.getFirstName();
+        assertThat(actual).isEqualTo(expected);
+    }
 }
